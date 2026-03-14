4ccb1bd6ad8704b63804252700ebefb66ef90620
Abdul Mateen Anis Miyanoor <161159851+AbdulMateenMiyanoor0711@users.noreply.github.com>
2026-03-14 14:07:15 +0530

    feat: update quiz questions by re-adding previously commented out questions and fix server port number

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 808;
+const port = 8080;
 
+app.get("/", (res, req) => {
+  console.log("hii");
+});
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
