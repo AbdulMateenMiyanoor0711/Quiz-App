01db8684f4fd0a26ecb8f7e3e40f9d5381a3e7fd
Abdul Mateen Anis Miyanoor <161159851+AbdulMateenMiyanoor0711@users.noreply.github.com>
2026-03-15 11:04:42 +0530

    feat: implement initial database connection and setup routes for quiz API

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (res, req) => {
+app.get("/getquiz", (res, req) => {
   console.log("hii");
 });
+
+
+app.post("/addpost",(res,req)=>{
+  
+})
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
